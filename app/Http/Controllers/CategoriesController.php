1ceee128a52d5186e1e41ea561745c0652f6069d
Morris Mukiri <morrismukiri@gmail.com>
2016-09-23 16:09:03 +0300

    return single json object on categories and error if not found

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,21 +7,21 @@ class CategoriesController extends Controller {
   public function listAll(){
     $categories = Categories::where('extension','=','com_content')->where('published','=','1')->orderBy('lft', 'ASC')->get();
     
-    if($categories){
-      return response()->json(['data' => $categories], 200);
+    if(!$categories->isEmpty()){
+      return response()->json($categories, 200);
     }
     
-    return response()->json(['data' => 'Categories not found'], 404);
+    return response()->json(['error' => 'Categories not found'], 404);
   }
   
   public function getCategory($id){
     $category = Categories::find($id);
     
-    if($category){
-      return response()->json(['data' => $category], 200);
+    if(!is_null($category)){
+      return response()->json($category, 200);
     }
     
-    return response()->json(['data' => 'Category not found'], 404);
+    return response()->json(['error' => 'Category not found'], 404);
     
   }
   
